3fd34fcdc61ddff7da89dd6d76be9edb15a1504d
yaeba <xuankentay@gmail.com>
2021-02-04 14:42:30 +1100

    init

---
+++ generate_readme.py
@@ -0,0 +1,72 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+headers = {
+    "content-type": "application/json",
+    "accept": "*/*",
+}
+
+
+def get_params(page):
+    return (("list", ""), ("orderBy", "id"), ("page", f"{page}"))
+
+
+page = 0
+
+all = []
+while True:
+    response = requests.get(
+        "https://binarysearch.com/api/questionlist",
+        headers=headers,
+        params=get_params(page),
+    )
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    json_dict = json.loads(soup.text)
+
+    questions = [(x["id"], x["slug"], x["difficulty"]) for x in json_dict["questions"]]
+
+    if len(questions) == 0:
+        break
+
+    all.extend(questions)
+    page += 1
+
+all = list(set(all))
+all.sort()
+
+
+def get_difficulty(idx):
+    if idx == 0:
+        return ":green_circle:"
+    elif idx == 1:
+        return ":orange_circle:"
+    elif idx == 2:
+        return ":red_circle:"
+    else:
+        return ":large_blue_circle:"
+
+
+base_url = "https://binarysearch.com/problems"
+title = "# Binary Search Solutions"
+color_legend = """
+- :green_circle:: **Easy**
+- :orange_circle:: **Medium**
+- :red_circle:: **Hard**
+- :large_blue_circle:: **Harder**
+"""
+table_headers = "[]() | Id | Question | Difficulty | Solution | Notes"
+table_border = ":---: | :---: | --- | :---: | :---: | ---"
+
+with open("README.md", "w") as fp:
+    fp.write(title + "\n\n")
+    fp.write(color_legend + "\n")
+    fp.write(table_headers + "\n")
+    fp.write(table_border + "\n")
+    for (id, slug, difficulty_idx) in all:
+        name = slug.replace("-", " ")
+        fp.write(
+            f"[](check) | {id} | [{name}]({base_url}/{slug}) | {get_difficulty(difficulty_idx)} | [](solution) | [](notes) \n"
+        )
